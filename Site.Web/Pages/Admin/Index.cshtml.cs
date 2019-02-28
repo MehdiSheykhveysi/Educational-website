a39bc55dca922ec144f54d328c8bd34b3790827f
Mehdi Sheykhveysi <m.sheykhveysi4680@gmail.com>
2019-02-28 21:27:00 +0330

    Admin Panel

---
+++ Site.Web/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Site.Web.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+           
+        }
+    }
+}
